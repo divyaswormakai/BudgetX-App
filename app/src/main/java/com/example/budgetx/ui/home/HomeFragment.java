bcfe6d4c2cbb2f542c57e11cfcaeb416af5749c2
Divyaswor Makai <makaidivya@gmail.com>
2019-10-21 15:21:20 +0545

    Added dialog popup fragments

--- app/src/main/java/com/example/budgetx/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/budgetx/ui/home/HomeFragment.java
@@ -1,28 +1,40 @@
 package com.example.budgetx.ui.home;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.budgetx.R;
+import com.example.budgetx.ui.ExpenseBottomSheet;
+import com.example.budgetx.ui.IncomeBottomSheet;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends BottomSheetDialogFragment {
+
 
     private HomeViewModel homeViewModel;
+    private LayoutInflater inflator;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
+    public View onCreateView(@NonNull final LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        inflator = inflater;
         final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
@@ -30,6 +42,25 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        Button addInc = (Button) root.findViewById(R.id.inc);
+        Button addExp = (Button) root.findViewById(R.id.exp);
+        addInc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IncomeBottomSheet incomesheet = new IncomeBottomSheet();
+                incomesheet.show(getFragmentManager(),"BottomSheet");
+            }
+        });
+
+        addExp.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                ExpenseBottomSheet expensesheet = new ExpenseBottomSheet();
+                expensesheet.show(getFragmentManager(),"BottomSheet");
+            }
+        });
+
         return root;
     }
 }
