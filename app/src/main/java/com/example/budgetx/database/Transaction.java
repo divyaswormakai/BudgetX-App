6a93ce85d7a47bd54e91459a09fc7b867399cc70
Divyaswor Makai <makaidivya@gmail.com>
2019-11-02 11:37:29 +0545

    Added database handler 2

---
+++ app/src/main/java/com/example/budgetx/database/Transaction.java
@@ -0,0 +1,62 @@
+package com.example.budgetx.database;
+
+public class Transaction {
+    int transactionID;
+    int transactionCategoryID;
+    int categoryID;
+    int frequencyID;
+    float amount;
+    String description;
+
+    public Transaction(){
+
+    }
+
+    public Transaction(int transactioncat,int catID, int frequencyID, float amt,String descp){
+        this.transactionCategoryID = transactioncat;
+        this.categoryID = catID;
+        this.frequencyID = frequencyID;
+        this.amount = amt;
+        this.description = descp;
+    }
+
+    public void SetValues(int transactionID, int transactionCategoryID, int categoryID, int frequencyID, float amount, String description){
+        this.transactionID = transactionID;
+        this.transactionCategoryID = transactionCategoryID;
+        this.categoryID = categoryID;
+        this.frequencyID = frequencyID;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    public void SetTranscationID(int id){
+        this.transactionID = id;
+    }
+
+    public int GetTransactionID(){
+        return this.transactionID;
+    }
+
+    public int GetTransactionType(){
+        return this.transactionCategoryID;
+    }
+
+    public int GetCatID()
+    {
+        return this.categoryID;
+    }
+
+    public int GetFrequencyID(){
+        return this.frequencyID;
+    }
+
+    public float GetAmount()
+    {
+        return this.amount;
+    }
+
+    public String GetDescription()
+    {
+        return this.description;
+    }
+}
