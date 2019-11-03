b8fae51707d57425d81475aedc39c99d4b066c36
Divyaswor Makai <makaidivya@gmail.com>
2019-11-03 14:15:45 +0545

    Adding to database from views complete

--- app/src/main/java/com/example/budgetx/ui/ExpenseBottomSheet.java
+++ app/src/main/java/com/example/budgetx/ui/ExpenseBottomSheet.java
@@ -7,15 +7,28 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.example.budgetx.R;
+import com.example.budgetx.database.MyDBHandler;
+import com.example.budgetx.database.Transaction;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class ExpenseBottomSheet extends BottomSheetDialogFragment {
+    View v;
+    Spinner catSpinner;
+    EditText amtText,description;
+    RadioGroup freqRadio;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -26,6 +39,11 @@ public class ExpenseBottomSheet extends BottomSheetDialogFragment {
         dropdown.setAdapter(adapter);
 
         Button cancel = v.findViewById(R.id.cancel);
+        Button addBtn = v.findViewById(R.id.add);
+        catSpinner = v.findViewById(R.id.spinner);
+        freqRadio = v.findViewById(R.id.freq);
+        amtText = v.findViewById(R.id.amount);
+        description = v.findViewById(R.id.description);
         final Dialog dialog = getDialog();
 
         cancel.setOnClickListener(new View.OnClickListener(){
@@ -34,7 +52,56 @@ public class ExpenseBottomSheet extends BottomSheetDialogFragment {
                 dialog.cancel();
             }
         });
+
+        addBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                AddExpense(v);
+            }
+        });
         return v;
+    }
+
+    void AddExpense(View v){
+        Dialog dialog = getDialog();
+        MyDBHandler db = new MyDBHandler(v.getContext(),null);
+        String type,cat, freq,desc;
+        float amt;
+        type="Expense";
+        cat = catSpinner.getSelectedItem().toString();
+        switch(freqRadio.getCheckedRadioButtonId()){
+            case R.id.Daily:{
+                freq = "Daily";
+                break;
+            }
+            case R.id.Weekly:{
+                freq = "Weekly";
+                break;
+            }
+            case R.id.Monthly:{
+                freq = "Monthly";
+                break;
+            }
+            default:{
+                freq = "One Time";
+                break;
+            }
+        }
+        amt = Float.parseFloat(amtText.getText().toString());
+        desc = description.getText().toString();
+        Date c = Calendar.getInstance().getTime();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        String formattedDate = simpleDateFormat.format(c);
+        if(cat.length()>0 || freq.length() >0 || amt>0){
+            Transaction trans = new Transaction(type,cat,freq,amt,desc,formattedDate);
+            db.addHandler(trans);
+            dialog.cancel();
+
+        }
+        else{
+            Toast toast = Toast.makeText(getContext(),"One of the mandatory field is empty",Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 
 
--- app/src/main/java/com/example/budgetx/ui/MainBottomSheet.java
+++ app/src/main/java/com/example/budgetx/ui/MainBottomSheet.java
@@ -7,30 +7,52 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.example.budgetx.R;
+import com.example.budgetx.database.MyDBHandler;
+import com.example.budgetx.database.Transaction;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class MainBottomSheet extends BottomSheetDialogFragment {
-    Button cancel;
+    Button cancel,addBtn;
     Spinner dropdown;
     RadioButton inc,exp;
+    RadioGroup radioGroup,freqRadio;
+    Spinner catSpinner;
+    EditText amtText,description;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.dialog_main,container,false);
 
-        dropdown = v.findViewById(R.id.spinner);
+        radioGroup = v.findViewById(R.id.type);
+        catSpinner = v.findViewById(R.id.spinner);
+        freqRadio = v.findViewById(R.id.freq);
+        amtText = v.findViewById(R.id.amount);
+        description = v.findViewById(R.id.description);
         cancel = v.findViewById(R.id.cancel);
         inc = v.findViewById(R.id.incRadio);
         exp = v.findViewById(R.id.expRadio);
+        addBtn = v.findViewById(R.id.add);
 
         Clickable();
+
+        ArrayAdapter<CharSequence> adapter;
+        adapter = ArrayAdapter.createFromResource(getContext(), R.array.income_categories, R.layout.support_simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        catSpinner.setAdapter(adapter);
 
         return v;
     }
@@ -57,6 +79,60 @@ public class MainBottomSheet extends BottomSheetDialogFragment {
                 onRadioButtonClicked(v);
             }
         });
+
+        addBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                MyDBHandler db = new MyDBHandler(v.getContext(),null);
+                String type,cat, freq,desc;
+                float amt;
+
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+                if(selectedId == inc.getId()){
+                    type="Income";
+                }
+                else if(selectedId == exp.getId()){
+                    type="Expense";
+                }
+                else{
+                    type="";
+                }
+                cat = catSpinner.getSelectedItem().toString();
+                switch(freqRadio.getCheckedRadioButtonId()){
+                    case R.id.Daily:{
+                        freq = "Daily";
+                        break;
+                    }
+                    case R.id.Weekly:{
+                        freq = "Weekly";
+                        break;
+                    }
+                    case R.id.Monthly:{
+                        freq = "Monthly";
+                        break;
+                    }
+                    default:{
+                        freq = "One Time";
+                        break;
+                    }
+                }
+                amt = Float.parseFloat(amtText.getText().toString());
+                desc = description.getText().toString();
+                Date c = Calendar.getInstance().getTime();
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd");
+                String formattedDate = simpleDateFormat.format(c);
+                if(type.length()>0 || cat.length()>0 || freq.length() >0 || amt>0){
+                    Transaction trans = new Transaction(type,cat,freq,amt,desc,formattedDate);
+                    db.addHandler(trans);
+                    dialog.cancel();
+                }
+                else{
+                    Toast toast = Toast.makeText(getContext(),"One of the mandatory field is empty",Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+
+            }
+        });
     }
 
     public void onRadioButtonClicked(View v){
@@ -76,7 +152,6 @@ public class MainBottomSheet extends BottomSheetDialogFragment {
                     dropdown.setAdapter(adapter);
                     break;
                 }
-
             }
 
         }
